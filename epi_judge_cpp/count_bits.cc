f956a1ae944ea3c936dec1493c41be85d3960642
Kazuya Sakakihara <kazuya@violentlyhappy.org>
2019-09-16 21:24:41 -0700

    Solve count_bits.

--- epi_judge_cpp/count_bits.cc
+++ epi_judge_cpp/count_bits.cc
@@ -1,8 +1,31 @@
 #include "test_framework/generic_test.h"
 
+static short rs[256];
+
+static short count(int n) {
+  short c = 0;
+  while (n) {
+    if (n & 1) {
+      ++c;
+    }
+    n >>= 1;
+  }
+  return c;
+}
+static void fill_rs() {
+  for (int i = 0; i < 256; ++i) {
+    rs[i] = count(i);
+  }
+}
+
 short CountBits(unsigned int x) {
-  // TODO - you fill in here.
-  return 0;
+  fill_rs();
+  short c = 0;
+  for (int i = 0; x && i < sizeof(x); ++i) {
+    c += count(x & 0xFF);
+    x >>= 8;
+  }
+  return c;
 }
 
 int main(int argc, char* argv[]) {
