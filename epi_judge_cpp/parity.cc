cf502344c1f675748e23cc3c0ea314c182dd5040
Kazuya Sakakihara <kazuya@violentlyhappy.org>
2019-09-16 21:36:09 -0700

    Solve parity.

--- epi_judge_cpp/parity.cc
+++ epi_judge_cpp/parity.cc
@@ -1,7 +1,28 @@
 #include "test_framework/generic_test.h"
+
+static short ps[256];
+
+static short count(int n) {
+  short c = 0;
+  while (n) {
+    c += n & 1;
+    n >>= 1;
+  }
+  return c;
+}
+static void fill_ps() {
+  for (int i = 0; i < 256; ++i) {
+    ps[i] = count(i) & 1;
+  }
+}
 short Parity(unsigned long long x) {
-  // TODO - you fill in here.
-  return 0;
+  fill_ps();
+  short p = 0;
+  while (x) {
+    p ^= ps[x & 0xff];
+    x >>= 8;
+  }
+  return p;
 }
 
 int main(int argc, char* argv[]) {
