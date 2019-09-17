d93516e920529d6316d3771af80d40c17d36e9e9
Kazuya Sakakihara <kazuya@violentlyhappy.org>
2019-09-16 22:17:55 -0700

    Solve swap_bits.

--- epi_judge_cpp/swap_bits.cc
+++ epi_judge_cpp/swap_bits.cc
@@ -1,7 +1,12 @@
 #include "test_framework/generic_test.h"
 long long SwapBits(long long x, int i, int j) {
-  // TODO - you fill in here.
-  return 0;
+  long long b_i = (x >> i) & 1;
+  long long b_j = (x >> j) & 1;
+  if (b_i ^ b_j) {
+    long long mask = ~((1ll << i) | (1ll << j));
+    return (x & mask) | (b_j << i) | (b_i << j);
+  }
+  return x;
 }
 
 int main(int argc, char* argv[]) {
