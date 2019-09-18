aae396cffbd657241864e47a07af2ca943a0dcfe
Kazuya Sakakihara <kazuya@violentlyhappy.org>
2019-09-17 23:40:32 -0700

    Add primitive_multiply.

--- epi_judge_cpp/primitive_multiply.cc
+++ epi_judge_cpp/primitive_multiply.cc
@@ -1,7 +1,25 @@
 #include "test_framework/generic_test.h"
+
+unsigned long long Add(unsigned long long x, unsigned long long y) {
+  unsigned long long z = x ^ y;
+  unsigned long long c = (x & y) << 1;
+  while (c) {
+    unsigned long long zz = z ^ c;
+    c = (z & c) << 1;
+    z = zz;
+  }
+  return z;
+}
+
 unsigned long long Multiply(unsigned long long x, unsigned long long y) {
-  // TODO - you fill in here.
-  return 0;
+  unsigned long long a = 0;
+  for (int i = 0; i < sizeof(unsigned long long)*8; ++i) {
+    if (x & (1ULL << i)) {
+      a = Add(a, y << i);
+    }
+  }
+
+  return a;
 }
 
 int main(int argc, char* argv[]) {
