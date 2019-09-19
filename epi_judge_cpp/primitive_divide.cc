0e98f14dca68ec68c5e5c1ddb8e0f83de74d18e1
Kazuya Sakakihara <kazuya@violentlyhappy.org>
2019-09-18 20:52:15 -0700

    Add primitive_divide.

--- epi_judge_cpp/primitive_divide.cc
+++ epi_judge_cpp/primitive_divide.cc
@@ -1,7 +1,35 @@
 #include "test_framework/generic_test.h"
-int Divide(int x, int y) {
-  // TODO - you fill in here.
-  return 0;
+
+unsigned long long Add(unsigned long long x, unsigned long long y) {
+  unsigned long long z = x ^ y;
+  unsigned long long c = (x & y) << 1;
+  while (c) {
+    unsigned long long zz = z ^ c;
+    c = (z & c) << 1;
+    z = zz;
+  }
+  return z;
+}
+
+unsigned Subtract(unsigned long long x, unsigned long long y, unsigned &b) {
+  unsigned long long yy = Add(~y, 1);
+  unsigned long long z = Add(x, yy);
+  b = (z & (1ULL << 63)) ? 1 : 0;
+  return z;
+}
+
+unsigned Divide(unsigned x, unsigned y) {
+  unsigned q = 0;
+  for (int i = 31; i >= 0; --i) {
+    unsigned long long yy = (unsigned long long)y << i;
+    unsigned b;
+    unsigned z = Subtract(x, yy, b);
+    if (!b) {
+      x = z;
+      q = Add(q, 1U << i);
+    }
+  }
+  return q;
 }
 
 int main(int argc, char* argv[]) {
