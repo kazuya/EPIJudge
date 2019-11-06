3c41094295ddf39afd3754da80ab51bc8a3732c4
Kazuya Sakakihara <kazuya@violentlyhappy.org>
2019-11-06 00:12:30 -0800

    Add buy_and_sell_stock.

--- epi_judge_cpp/buy_and_sell_stock.cc
+++ epi_judge_cpp/buy_and_sell_stock.cc
@@ -2,8 +2,19 @@
 #include "test_framework/generic_test.h"
 using std::vector;
 double BuyAndSellStockOnce(const vector<double>& prices) {
-  // TODO - you fill in here.
-  return 0.0;
+  double m = 0;
+  double dm = 0;
+  for (int i = prices.size()-1; i >= 0; --i) {
+    if (prices[i] > m) {
+      m = prices[i];
+    } else {
+      double d = m - prices[i];
+      if (d > dm) {
+        dm = d;
+      }
+    }
+  }
+  return dm;
 }
 
 int main(int argc, char* argv[]) {
